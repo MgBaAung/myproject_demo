b2be657fc97da8f381c43b471721f739c0963f35
bytepulay <linn6802@gmail.com>
2022-09-19 19:47:16 +0630

    login logout done

--- myproject/lib/src/helper/buildcontext_extension.dart
+++ myproject/lib/src/helper/buildcontext_extension.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 extension BuildContextExtension on BuildContext {
   showError(String error) {
-    ScaffoldMessenger.of(this).showSnackBar(SnackBar(content: Text(error)));
+    Future.delayed(Duration.zero, () {
+      ScaffoldMessenger.of(this).showSnackBar(SnackBar(content: Text(error)));
+    });
   }
 }
